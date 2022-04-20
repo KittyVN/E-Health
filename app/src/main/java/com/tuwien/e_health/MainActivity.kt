6f8e87855e3c31963f6d7fd6644d8197d809025e
Daniel Schermann <e11777772@student.tuwien.ac.at>
2022-04-20 18:37:25 +0200

    added google fit permission

--- app/src/main/java/com/tuwien/e_health/MainActivity.kt
+++ app/src/main/java/com/tuwien/e_health/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.tuwien.e_health
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.ContentValues.TAG
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.fitness.Fitness
+import com.google.android.gms.fitness.FitnessOptions
+import com.google.android.gms.fitness.data.DataType
+import com.google.android.gms.fitness.request.DataReadRequest
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
 }
