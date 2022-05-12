5f03907e7d48093ec6145e998bbd50464f6690b8
Niklas Schuster <e11913637@student.tuwien.ac.at>
2022-05-12 18:33:57 +0200

    fixed settings activity button message and permission request

--- app/src/main/java/com/tuwien/e_health/SettingsActivity.kt
+++ app/src/main/java/com/tuwien/e_health/SettingsActivity.kt
@@ -1,18 +1,38 @@
 package com.tuwien.e_health
 
+import android.content.ContentValues
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.WindowManager
 import android.widget.Button
 import android.widget.Toast
 import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.auth.api.signin.GoogleSignInOptionsExtension
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.fitness.FitnessOptions
+import com.google.android.gms.fitness.data.DataType
+import com.google.android.gms.tasks.Task
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_settings.*
 
 class SettingsActivity : AppCompatActivity() {
 
+    private val fitnessOptions: GoogleSignInOptionsExtension = FitnessOptions.builder()
+        .addDataType(DataType.TYPE_HEART_RATE_BPM, FitnessOptions.ACCESS_READ)
+        .addDataType(DataType.TYPE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
+        .addDataType(DataType.TYPE_ACTIVITY_SEGMENT, FitnessOptions.ACCESS_READ)
+        .addDataType(DataType.TYPE_ACTIVITY_SEGMENT, FitnessOptions.ACCESS_WRITE)
+        .addDataType(DataType.TYPE_WORKOUT_EXERCISE, FitnessOptions.ACCESS_READ)
+        .build()
+
     private val RC_SIGNIN = 0
+    private val RC_PERMISSION = 1
+    private var testCounter = 0
+    private var average6hHeartRate = 0.0;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +43,12 @@ class SettingsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_settings)
 
         val textBtnSignInOut: Button = findViewById(R.id.btnSignInOut) as Button
+
+        if(GoogleSignIn.getLastSignedInAccount(this) == null) {
+            textBtnSignInOut.text = "Log in"
+        } else if (GoogleSignIn.getLastSignedInAccount(this) != null) {
+            textBtnSignInOut.text = "Log out"
+        }
 
         btnSignInOut.setOnClickListener {
             if (GoogleSignIn.getLastSignedInAccount(this) == null) {
@@ -35,6 +61,29 @@ class SettingsActivity : AppCompatActivity() {
             }
         }
 
+    }
+
+    private fun reqPermissions() {
+        // request Permissions specified in fitnessOptions
+
+        GoogleSignIn.requestPermissions(
+            this,
+            RC_PERMISSION,
+            getGoogleAccount(),
+            fitnessOptions)
+    }
+
+    private fun accountInfo() {
+        // show logged in email and id
+
+        val acct = GoogleSignIn.getLastSignedInAccount(this)
+        if (acct != null) {
+            Log.i(ContentValues.TAG, "account signed in")
+            Log.i(ContentValues.TAG, "personEmail: " + acct.email)
+            Log.i(ContentValues.TAG, "personId: " + acct.id)
+        }else{
+            Log.i(ContentValues.TAG, "no account")
+        }
     }
 
     private fun signIn() {
@@ -55,5 +104,34 @@ class SettingsActivity : AppCompatActivity() {
             .build()
         val mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
         mGoogleSignInClient.signOut()
+    }
+
+    private fun oAuthPermissionsApproved() =
+        GoogleSignIn.hasPermissions(GoogleSignIn.getLastSignedInAccount(this), fitnessOptions)
+
+    private fun getGoogleAccount(): GoogleSignInAccount =
+        GoogleSignIn.getAccountForExtension(this, fitnessOptions)
+
+    // gets automatically called after sending login-request
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode === RESULT_OK) {
+            if(!oAuthPermissionsApproved()){
+                // request missing Permissions
+                reqPermissions()
+            }
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+            Toast.makeText(this, "Signed In", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account = completedTask.getResult(ApiException::class.java)
+            accountInfo()
+        } catch (e: ApiException) {
+            Log.w(ContentValues.TAG, "signInResult:failed code=" + e.statusCode)
+        }
     }
 }
