b671581f2a9a802ac55213e60ab16159acbdfe74
GreatPotatoCat <43379461+NiklasSchuster@users.noreply.github.com>
2022-05-26 18:01:37 +0200

    Added Google Fit Stuff for WearOS

--- wearOS/src/main/java/com/tuwien/e_health/MainActivity.kt
+++ wearOS/src/main/java/com/tuwien/e_health/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.tuwien.e_health
 
+import android.Manifest
 import android.app.Activity
+import android.bluetooth.BluetoothDevice
+import android.companion.CompanionDeviceManager
 import android.os.Bundle
 
 import android.content.BroadcastReceiver
@@ -8,16 +11,50 @@ import android.content.ContentValues.TAG
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.content.pm.PackageManager
 import android.util.Log
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.auth.api.signin.GoogleSignInOptionsExtension
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.fitness.Fitness
+import com.google.android.gms.fitness.FitnessOptions
+import com.google.android.gms.fitness.data.DataSource
+import com.google.android.gms.fitness.data.DataType
+import com.google.android.gms.fitness.request.DataSourcesRequest
+import com.google.android.gms.fitness.request.OnDataPointListener
+import com.google.android.gms.fitness.request.SensorRequest
+import com.google.android.gms.tasks.Task
 import com.google.android.gms.tasks.Tasks
 import com.google.android.gms.wearable.Wearable
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.concurrent.ExecutionException
+import java.util.concurrent.TimeUnit
 
 
 class MainActivity : Activity() {
+
+    private val fitnessOptions: GoogleSignInOptionsExtension = FitnessOptions.builder()
+        .addDataType(DataType.TYPE_HEART_RATE_BPM, FitnessOptions.ACCESS_READ)
+        .addDataType(DataType.TYPE_HEART_RATE_BPM, FitnessOptions.ACCESS_WRITE)
+        .addDataType(DataType.TYPE_ACTIVITY_SEGMENT, FitnessOptions.ACCESS_READ)
+        .addDataType(DataType.TYPE_ACTIVITY_SEGMENT, FitnessOptions.ACCESS_WRITE)
+        .addDataType(DataType.TYPE_LOCATION_SAMPLE, FitnessOptions.ACCESS_READ)
+        .addDataType(DataType.TYPE_LOCATION_SAMPLE, FitnessOptions.ACCESS_WRITE)
+        .addDataType(DataType.TYPE_WORKOUT_EXERCISE, FitnessOptions.ACCESS_READ)
+        .build()
+
+    private val RC_SIGNIN = 0
+    private val RC_PERMISSION = 1
+    private var testCounter = 0
+    private var restingHeartRate = -1.0
+    private val SELECT_DEVICE_REQUEST_CODE = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +70,45 @@ class MainActivity : Activity() {
         val newFilter = IntentFilter(Intent.ACTION_SEND)
         val messageReceiver = Receiver()
         LocalBroadcastManager.getInstance(this).registerReceiver(messageReceiver, newFilter)
+
+        // check for android permissions
+        checkPermissions()
+    }
+
+    private fun checkPermissions() {
+        // check for android permissions
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACTIVITY_RECOGNITION)
+            != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACTIVITY_RECOGNITION),
+                RC_PERMISSION
+            )
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                RC_PERMISSION
+            )
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.BODY_SENSORS)
+            != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.BODY_SENSORS),
+                RC_PERMISSION
+            )
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT)
+            != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.BLUETOOTH_CONNECT),
+                RC_PERMISSION
+            )
+        }
     }
 
     // receive message from smartphone
@@ -40,6 +116,137 @@ class MainActivity : Activity() {
         override fun onReceive(context: Context, intent: Intent) {
             Log.i(TAG, "Incoming msg: " + intent.getStringExtra("message").toString())
         }
+    }
+
+    private fun signIn() {
+        // log in with Google Account
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+        val mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+        val signInIntent = mGoogleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGNIN)
+    }
+
+    private fun logOut() {
+        // log out of Google Account
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .build()
+        val mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+        mGoogleSignInClient.signOut()
+    }
+
+    private fun reqPermissions() {
+        // request Permissions specified in fitnessOptions
+
+        GoogleSignIn.requestPermissions(
+            this,
+            RC_PERMISSION,
+            getGoogleAccount(),
+            fitnessOptions)
+    }
+
+    private fun accountInfo() {
+        // show logged in email and id
+
+        val acct = GoogleSignIn.getAccountForExtension(this, fitnessOptions)
+        if (acct != null) {
+            Log.i(TAG, "account signed in")
+            Log.i(TAG, "personEmail: " + acct.email)
+            Log.i(TAG, "personName: " + acct.displayName)
+            Log.i(TAG, "personId: " + acct.id)
+        }else{
+            Log.i(TAG, "no account")
+        }
+    }
+
+    private fun oAuthPermissionsApproved() =
+        GoogleSignIn.hasPermissions(GoogleSignIn.getAccountForExtension(this, fitnessOptions), fitnessOptions)
+
+    private fun getGoogleAccount(): GoogleSignInAccount =
+        GoogleSignIn.getAccountForExtension(this, fitnessOptions)
+
+    // gets automatically called after sending login-request
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode === RESULT_OK) {
+            if (!oAuthPermissionsApproved()) {
+                // request missing Permissions
+                reqPermissions()
+            }
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+            Toast.makeText(this, "Signed In", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account = completedTask.getResult(ApiException::class.java)
+            accountInfo()
+        } catch (e: ApiException) {
+            Log.w(TAG, "signInResult:failed code=" + e.statusCode)
+        }
+    }
+
+    private var dataPointListener: OnDataPointListener? = null
+
+    /** Finds available data sources and attempts to register on a specific [DataType].  */
+    private fun findFitnessDataSources() { // [START find_data_sources]
+        Log.i(TAG, "hier")
+        //tvEnd.setText("hier")
+        // Note: Fitness.SensorsApi.findDataSources() requires the ACCESS_FINE_LOCATION permission.
+        Fitness.getSensorsClient(this, getGoogleAccount())
+            .findDataSources(
+                DataSourcesRequest.Builder()
+                    .setDataTypes(DataType.TYPE_HEART_RATE_BPM)
+                    .setDataSourceTypes(DataSource.TYPE_RAW)
+                    .build())
+            .addOnSuccessListener { dataSources ->
+                for (dataSource in dataSources) {
+                    Log.i(TAG, "hi")
+                    //tvEnd.setText("hi")
+                    Log.i(TAG, "Data source found: $dataSource")
+                    Log.i(TAG, "Data Source type: " + dataSource.dataType.name)
+                    // Let's register a listener to receive Activity data!
+                    if (dataSource.dataType == DataType.TYPE_HEART_RATE_BPM && dataPointListener == null) {
+                        Log.i(TAG, "Data source found!  Registering.")
+                        registerFitnessDataListener(dataSource, DataType.TYPE_HEART_RATE_BPM)
+                    }
+                }
+                Log.i(TAG, "hie1r $dataSources")
+            }
+            .addOnFailureListener { e -> Log.e(TAG, "failed", e) }
+    }
+
+    private fun registerFitnessDataListener(dataSource: DataSource, dataType: DataType) {
+        // [START register_data_listener]
+        //tvEnd.setText("yoo")
+        var dataPointListener = OnDataPointListener { dataPoint ->
+            for (field in dataPoint.dataType.fields) {
+                val value = dataPoint.getValue(field)
+                //tvSteps.setText("steps: $value")
+                Log.i(TAG, "Detected DataPoint field: ${field.name}")
+                Log.i(TAG, "Detected DataPoint value: $value")
+            }
+        }
+        Fitness.getSensorsClient(this, getGoogleAccount())
+            .add(
+                SensorRequest.Builder()
+                    .setDataSource(dataSource) // Optional but recommended for custom data sets.
+                    .setDataType(dataType) // Can't be omitted.
+                    .setSamplingRate(2, TimeUnit.SECONDS)
+                    .build(),
+                dataPointListener)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Log.i(TAG, "Listener registered!")
+                } else {
+                    Log.e(TAG, "Listener not registered.", task.exception)
+                }
+            }
     }
 
     // send message to smartphone
